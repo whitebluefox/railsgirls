06148640e14fb2bf1a199b5da7dc05f2507fd9e3
whitebluefox <kalinkina.nika@mail.ru>
2019-11-02 15:27:12 +0300

    Adding Authentication with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
-  root to: redirect('/ideas')
+  root to: 'ideas#index'
   get "pages/info"
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
